8168b44affdea6a029f8a7a64f903d9b49c5b089
JoannaLigenza <ligenzajoanna@gmail.com>
2019-07-22 15:34:50 +0200

    divided main site to two columns and added button and dropdown elements

--- index.php
+++ index.php
@@ -79,54 +79,82 @@
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <div class="d-flex flex-column flex-sm-row justify-content-between border-bottom border-warning">
-        <a class="navbar-brand p-3" href="/">LOGO HERE</a>
-        <div class="p-2 ">
-            <button type="button" class="btn btn-outline-warning float-right"><?php echo $lang["log_in"]  ?></button>
-            <button type="button" class="btn btn-warning float-right mx-2"><?php echo $lang["sign_up"]  ?></button>
+    <div class="container-fluid p-0">
+        <!-- HEADER -->
+        <header>
+            <div class="d-flex flex-column flex-sm-row justify-content-between border-bottom border-warning">
+                <a class="navbar-brand p-3" href="/">LOGO HERE</a>
+                <div class="p-2 float-right">
+                    <button type="button" class="btn btn-outline-warning"><?php echo $lang["log_in"]  ?></button>
+                    <button type="button" class="btn btn-warning mx-2 myBtnHover"><?php echo $lang["sign_up"]  ?></button>
+                </div>
+            </div>
+
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav justify-content-center text-center m-auto">
+                    <li class="nav-item nav-fill">
+                        <a class="nav-link active" href="#"><?php echo $lang["all"]  ?> <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Html</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Css</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Javascript</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- HEADER END -->
+
+        <!-- MAIN  -->
+        <div class="container-fluid">
+            <div class="row justify-content-center my-4 mx-0">
+                <!-- LEFT COL -->
+                <main class="col-md-7 col-xl-6">
+
+                    <div class="d-flex flex-row justify-content-end">
+                        <div class="container-flex justify-content-center">
+                            <button type="button" class="btn btn-warning my-2 shadow-none myBtnHover"> <?php echo $lang["add_question"]  ?> </button>
+                        </div>
+                    </div>
+
+                    <div class="my-3">
+                        <div class="d-flex flex-row justify-content-end">
+                            <ul class="nav nav-tabs">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sortowanie:</a>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="#">Najlepiej oceniane</a>
+                                    <a class="dropdown-item" href="#">Z odpowiedziami</a>
+                                    <a class="dropdown-item" href="#">Data dodania</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+
+                </main>
+
+                <!-- RIGHT COL -->
+                <div class="col-md-4 col-xl-3 border-left border-warning">
+                    asd
+                </div>
+
+            </div>
         </div>
+        <!-- MAIN END -->
+
+
     </div>
-
-    <!-- <div class="nav bg-light">
-        <nav class="navbar navbar-expand-sm navbar-light red">
-            <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNav" >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse nav-fill" id="myNav">
-                <a class="nav-item nav-link" href="#">Active</a>
-                <a class="nav-item nav-link" href="#">Link</a>
-                <a class="nav-item nav-link" href="#">Link</a>
-                <a class="nav-item nav-link" href="#">Disabled</a>
-            </div>
-            </div>
-        </nav>
-    </div> -->
-
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav text-center m-auto">
-            <li class="nav-item active ">
-                <a class="nav-link" href="#"><?php echo $lang["all"]  ?> <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Html</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Css</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Javascript</a>
-            </li>
-            </ul>
-        </div>
-    </nav>
-
     
     <!-- 
     <h1><?php echo $lang["title"]  ?></h1>
@@ -137,6 +165,13 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+
+    <script> 
+        // Switch tabs
+        $('#myTab a').on('click', function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        });
+</script>
 </body>
 </html>
