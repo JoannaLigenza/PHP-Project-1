f2704f9b535ca82cf2f5e4cdb9bb22f4c66c49d6
JoannaLigenza <ligenzajoanna@gmail.com>
2019-08-06 21:55:08 +0200

    header and nav separated

---
+++ nav.php
@@ -0,0 +1,24 @@
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav justify-content-center text-center m-auto">
+                        <li class="nav-item nav-fill">
+                            <a class="nav-link active" href="#"><?php echo $lang["all"]  ?> <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Html</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Css</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Javascript</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><?php echo $lang["developer-tools"] ?></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
